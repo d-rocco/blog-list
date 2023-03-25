095bd607ae15db4eeb9b0b8881c01d4f412491d3
d-rocco <dhrocco27@gmail.com>
2023-03-25 12:01:19 -0400

    Refactored routers and models into own files

--- index.js
+++ index.js
@@ -4,47 +4,14 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const logger = require('./utils/logger');
 const config = require('./utils/config');
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-blogSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
-});
-
-const Blog = mongoose.model('Blog', blogSchema);
+const blogsRouter = require('./controllers/blogs');
 
 mongoose.connect(config.MONGODB_URI);
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/blogs', (request, response, next) => {
-  Blog.find({})
-    .then((blogs) => {
-      response.json(blogs);
-    })
-    .catch((error) => next(error));
-});
-
-app.post('/api/blogs', (request, response, next) => {
-  const blog = new Blog(request.body);
-
-  blog
-    .save()
-    .then((result) => {
-      response.status(201).json(result);
-    })
-    .catch((error) => next(error));
-});
+app.use('/api/blogs', blogsRouter);
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' });
