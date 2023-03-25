45504db22ba62024f8783fba32f76c20e5f1d02a
d-rocco <dhrocco27@gmail.com>
2023-03-25 12:21:33 -0400

    Refactored to separate app and index

--- index.js
+++ index.js
@@ -1,31 +1,6 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
-const logger = require('./utils/logger');
+const app = require('./app');
 const config = require('./utils/config');
-const blogsRouter = require('./controllers/blogs');
-const middleware = require('./utils/middleware');
-
-logger.info('connecting to', config.MONGODB_URI);
-
-mongoose
-  .connect(config.MONGODB_URI)
-  .then(() => {
-    logger.info('Connected to MongoDB');
-  })
-  .catch((error) => {
-    logger.error('error connecting to MongoDB', error.message);
-  });
-
-app.use(cors());
-app.use(express.json());
-
-app.use('/api/blogs', blogsRouter);
-
-app.use(middleware.unknownEndpoint);
-
-app.use(middleware.errorHandler);
+const logger = require('./utils/logger');
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
